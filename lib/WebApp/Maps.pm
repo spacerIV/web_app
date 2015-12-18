58ad9b1e029cb3604f4d1134a4012de8a6770ee5
Valcho Nedelchev <vulcho@gmail.com>
2015-12-18 00:06:25 +0000

    Pass template to render() and perltidy
    All links are working now.

--- lib/WebApp/Maps.pm
+++ lib/WebApp/Maps.pm
@@ -12,69 +12,78 @@ my $BB;
 sub weather {
     my $self = shift;
 
-    $self->render( title => 'Weather',
-                   msg   => 'Weather', 
-                   blurb => 'click somewhere to show live weather' );
+    $self->render(
+        template => 'maps/weather',
+        title    => 'Weather',
+        msg      => 'Weather',
+        blurb    => 'click somewhere to show live weather'
+    );
 }
 
 sub get_weather {
     my $self = shift;
 
     # eg: $location =~ (16.130262012034766, 31.201171875)
-    $self->param( 'location' ) =~ m/^\((?<lat>-?\d{1,3}\.\d+), (?<lng>-?\d{1,3}\.\d+)\)/; 
+    $self->param('location')
+        =~ m/^\((?<lat>-?\d{1,3}\.\d+), (?<lng>-?\d{1,3}\.\d+)\)/;
 
-    my ( $lat, $lng ) = ( $+{lat}, $+{lng} ); 
+    my ( $lat, $lng ) = ( $+{lat}, $+{lng} );
 
-    my $W = WWW::Wunderground::API->new( location  => $lat . ',' . $lng,
-                                         api_key  => '7c6a2de5aedd381b',
-                                         auto_api => 1, );
+    my $W = WWW::Wunderground::API->new(
+        location => $lat . ',' . $lng,
+        api_key  => '7c6a2de5aedd381b',
+        auto_api => 1,
+    );
     my %w = (
-        place           => $W->display_location->full,
-        temp_c          => $W->temp_c,
-        wind_condition  => $W->wind_string,
-        humidity        => $W->relative_humidity,
-        condition       => $W->weather,
-        icon            => $W->icon_url,
+        place          => $W->display_location->full,
+        temp_c         => $W->temp_c,
+        wind_condition => $W->wind_string,
+        humidity       => $W->relative_humidity,
+        condition      => $W->weather,
+        icon           => $W->icon_url,
     );
 
     # change icon set
-    $w{ icon } =~ s/\/i\/\/c\/\/k\//\/i\/\/c\/\/i\//g; 
+    $w{icon} =~ s/\/i\/\/c\/\/k\//\/i\/\/c\/\/i\//g;
 
-    my $ok = ( defined $w{ place } ) ? 1 : 0;
+    my $ok = ( defined $w{place} ) ? 1 : 0;
 
     $self->render(
         json => {
             lat     => $lat,
             lng     => $lng,
-            weather => \%w, 
-            success => $ok, } );
-} 
+            weather => \%w,
+            success => $ok,
+        }
+    );
+}
 
 sub borisbikes {
     my $self = shift;
 
-    $BB = WebService::BorisBikes->new( { refresh_rate => 120 } ) 
-        unless defined $BB && $BB->isa( 'WebService::BorisBikes' );
+    $BB = WebService::BorisBikes->new( { refresh_rate => 120 } )
+        unless defined $BB && $BB->isa('WebService::BorisBikes');
 
     my $stations = $BB->get_all_stations;
 
-    my $data;
+    my $data = {};
 
     foreach my $s ( keys $stations ) {
-        my ( $nbBikes, $nbDocks ) = ( $stations->{ $s }->get_nbBikes, $stations->{ $s }->get_nbDocks );
+        my ( $nbBikes, $nbDocks )
+            = ( $stations->{$s}->get_nbBikes, $stations->{$s}->get_nbDocks );
 
         next if $nbDocks == 0;
 
-        my $value =  $nbBikes / $nbDocks;
+        my $value = $nbBikes / $nbDocks;
 
         my $icon = 1 if ( $value >= 0.00 && $value < 0.33 );
-        $icon    = 2 if ( $value >= 0.33 && $value < 0.66 );
-        $icon    = 3 if ( $value >= 0.66 );
+        $icon = 2 if ( $value >= 0.33 && $value < 0.66 );
+        $icon = 3 if ( $value >= 0.66 );
 
-        $data->{ $s } = { 
-            lat         => $stations->{ $s }->get_lat,
-            long        => $stations->{ $s }->get_long,
-            name        => $stations->{ $s }->get_name,
+        $data->{$s} = {
+            lat         => $stations->{$s}->get_lat,
+            long        => $stations->{$s}->get_long,
+            name        => $stations->{$s}->get_name,
             nbBikes     => $nbBikes,
             nbDocks     => $nbDocks,
             icon_colour => $icon,
@@ -83,22 +92,22 @@ sub borisbikes {
 
     my $success = ( defined $data ) ? 1 : 0;
 
-    $self->render( 
-            title      => 'Boris Bikes',
-            msg        => 'Boris Bikes',
-            blurb      => '',
-            success    => $success, 
-            stations   => $data, 
+    $self->render(
+        title    => 'Boris Bikes',
+        msg      => 'Boris Bikes',
+        blurb    => '',
+        success  => $success,
+        stations => $data,
     );
 }
 
 sub get_bb_station_info {
     my $self = shift;
 
-    $BB = WebService::BorisBikes->new( { refresh_rate => 120 } ) 
-        unless defined $BB && $BB->isa( 'WebService::BorisBikes' );
+    $BB = WebService::BorisBikes->new( { refresh_rate => 120 } )
+        unless defined $BB && $BB->isa('WebService::BorisBikes');
 
-    my $s = $BB->get_station_by_id( $self->param( 'station_id' ) );
+    my $s = $BB->get_station_by_id( $self->param('station_id') );
 
     my %data = (
         name    => $s->get_name,
--- lib/WebApp/Misc.pm
+++ lib/WebApp/Misc.pm
@@ -11,19 +11,22 @@ use Astro::Constants::MKS qw(:long);
 sub schwarzschild {
     my $self = shift;
 
-    $self->render( title => 'Schwarzschild Radius', 
-                   msg   => 'Schwarzschild Radius', 
-                   blurb => '' ),
+    $self->render(
+        title => 'Schwarzschild Radius',
+        msg   => 'Schwarzschild Radius',
+        blurb => ''
+        ),
+        ;
 }
 
 sub calculate_schwarzschild_radius {
     my $self = shift;
 
-    my $mass = encode_entities $self->param( 'mass' );
- 
-    my $answer = 2 * GRAVITATIONAL * $mass / LIGHT_SPEED ** 2;
+    my $mass = encode_entities $self->param('mass');
 
-    $answer =~ s/e-(\d+)$/ x 10 <sup>-$1<\/sup>/; 
+    my $answer = 2 * GRAVITATIONAL * $mass / LIGHT_SPEED**2;
+
+    $answer =~ s/e-(\d+)$/ x 10 <sup>-$1<\/sup>/;
 
     $self->render( json => { schwarzschild_radius => $answer } );
 }
@@ -31,9 +34,13 @@ sub calculate_schwarzschild_radius {
 sub tetris {
     my $self = shift;
 
-    $self->render( title => 'WebGL Tetris', 
-                   msg   => 'WebGL Tetris', 
-                   blurb => 'Ported from C & OpenGL to Javascript using Emscripten' ),
+    $self->render(
+        template => 'misc/tetris',
+        title    => 'WebGL Tetris',
+        msg      => 'WebGL Tetris',
+        blurb    => 'Ported from C & OpenGL to Javascript using Emscripten'
+        ),
+        ;
 }
 
 sub csrf {
@@ -43,24 +50,28 @@ sub csrf {
     my $city = $self->req->body_params->param('city');
 
     unless ( defined $city ) {
-        $self->render( title => 'CSRF', 
-                       msg   => 'CSRF', 
-                       blurb => 'This form contains a hidden csrf token', );
+        $self->render(
+            template => 'misc/csrf',
+            title    => 'CSRF',
+            msg      => 'CSRF',
+            blurb    => 'This form contains a hidden csrf token',
+        );
         return;
-     }
+    }
 
     # Check CSRF token
     my $validation = $self->validation;
 
-    return $self->render(text => 'Bad CSRF token!', status => 403)
+    return $self->render( text => 'Bad CSRF token!', status => 403 )
         if $validation->csrf_protect->has_error('csrf_token');
 
     $city = $validation->required('city')->param('city');
 
-    $self->render( title => 'CSRF', 
-                   msg   => 'CSRF', 
-                   text  => "Low orbit ion cannon pointed at $city!" )
-        unless $validation->has_error;
+    $self->render(
+        title => 'CSRF',
+        msg   => 'CSRF',
+        text  => "Low orbit ion cannon pointed at $city!"
+    ) unless $validation->has_error;
 }
 
 1;
